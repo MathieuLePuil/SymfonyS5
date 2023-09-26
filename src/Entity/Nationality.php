7ca1e3e82501887775a86255d501756ee79df361
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-09-26 08:13:06 +0200

    feat: create Nationality entity

---
+++ src/Entity/Nationality.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NationalityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NationalityRepository::class)]
+class Nationality
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nationality = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNationality(): ?string
+    {
+        return $this->nationality;
+    }
+
+    public function setNationality(string $nationality): static
+    {
+        $this->nationality = $nationality;
+
+        return $this;
+    }
+}
