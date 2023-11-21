3872a6539126dba442a28f68e7bb551e50edb155
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-11-21 10:08:30 +0100

    fix: unversioned files

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class CategoryFixtures extends Fixture implements OrderedFixtureInterface
+{
+    // définir l'ordre de chargement des fixtures
+    public function getOrder(): int
+    {
+        return 3;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        // Vous pouvez modifier cette liste pour ajouter/supprimer des catégories
+        $categories = ['Action', 'Adventure', 'Comedy', 'Drama', 'Horror'];
+
+        for ($i = 0; $i < 5; ++$i) {
+            $category = new Category();
+            $category->setName((isset($categories[$i])) ? $categories[$i] : $faker->words(2, true));
+            $manager->persist($category);
+            $this->addReference('category_'.($i+1), $category);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(
+        protected UserPasswordHasherInterface $passwordHasherInterface
+    )
+    {
+    }
+
+    public function getOrder(): int
+    {
+        return 5;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 1; $i <= 5; ++$i) {
+            $user = new User();
+            $user->setUsername('user'.$i);
+            $user->setPassword($this->passwordHasherInterface->hashPassword($user, 'test'.$i));
+
+            $manager->persist($user);
+        }
+
+        $userAdmin = new User();
+        $userAdmin->setUsername('admin');
+        $userAdmin->setPassword($this->passwordHasherInterface->hashPassword($user, 'test'));
+        $userAdmin->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($userAdmin);
+
+        $manager->flush();
+    }
+}
