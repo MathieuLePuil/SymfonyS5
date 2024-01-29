6fa88960fcf58c952d26bbcf2adfe14e0ada5a3a
Mathieu Le Puil <lepuilmathieu@gmail.com>
2024-01-29 14:21:17 +0100

    feat: add me on api

--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -6,6 +6,7 @@ use ApiPlatform\Doctrine\Orm\Filter\BooleanFilter;
 use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
@@ -20,6 +21,8 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: MovieRepository::class)]
 #[ApiResource(
@@ -56,9 +59,10 @@ class Movie
     #[Assert\NotBlank(message: 'La description est obligatoire.')]
     private ?string $description = null;
 
-    #[ORM\Column(type: Types::TEXT)]
-    #[Groups(['movie:read'])]
-    private ?string $releaseDate = null;
+    /**
+     * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
+     */
+    private ?\DateTimeInterface $releaseDate;
 
     #[ORM\Column(length: 50)]
     #[Groups(['movie:read'])]
@@ -79,6 +83,17 @@ class Movie
 
     #[ORM\Column]
     private ?bool $online = null;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $fileName;
+
+    /**
+     * @Vich\UploadableField(mapping="movie", fileNameProperty="fileName")
+     * @var File|null
+     */
+    private $file;
 
     public function __construct()
     {
@@ -150,6 +165,32 @@ class Movie
         $this->category = $category;
 
         return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFileName()
+    {
+        return $this->fileName;
+    }
+
+    /**
+     * @param mixed $fileName
+     */
+    public function setFileName($fileName): void
+    {
+        $this->fileName = $fileName;
+    }
+
+    public function getFile(): ?File
+    {
+        return $this->file;
+    }
+
+    public function setFile(?File $file): void
+    {
+        $this->file = $file;
     }
 
     /**
